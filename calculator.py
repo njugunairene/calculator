2142a94837482cca23eb2420efa48d778762da99
ngujunairene <njokiirenenjuguna@gmail.com>
2019-04-07 13:16:10 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+def subtract(a,b):
+  c=a-b
+  print(c)
+
+def  multipy(a,b):
+  c=a*b
+  print(c)
+
+def division(a,b):
+  c=a/b
+  print(c)
+
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+def percent(a,b):
+	c=a/b*100
+	print(c)
+
+def Exponent(a,b):
+	c=a**b
+	print(c)
+
+def division_whole_number(a,b):
+	c=a//b
+	print(c)
+
+
+
